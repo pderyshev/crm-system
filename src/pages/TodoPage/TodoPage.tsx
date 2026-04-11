285d42fa39c6ceb584750336263f69d5d9feccfa
pDeryshev <p.deryshev@yandex.ru>
2026-04-11 09:53:33 +0300

    удалил кастомный хук, вынес логику в TodoPage

--- src/pages/TodoPage/TodoPage.tsx
+++ src/pages/TodoPage/TodoPage.tsx
@@ -1,27 +1,116 @@
 import { CreateTodo } from "../../components/CreateTodo/CreateTodo";
 import { TodoFilter } from "../../components/TodoFilter/TodoFilter";
 import { TodoListView } from "../../components/TodoList/TodoList";
-import { useTodoList } from "../../hooks/useTodoList";
+import {
+  getTodos,
+  createTodo,
+  deleteTodo,
+  updateTodo,
+} from "../../api/Todo";
 import "./todoPage.scss";
+import { useState, useEffect } from "react";
+import type { RequestState } from "../../types/requestState";
+import type { FilterTodo } from "../../types/todo";
 
 export default function TodoPage() {
-  const todo = useTodoList();
+  const [state, setState] = useState<RequestState>({ status: "idle" });
+  const [filter, setFilter] = useState<FilterTodo>("all")
+
+  // Загрузка списка задач
+  const loadTodoList = async (currentFilter = filter) => {
+    setState({ status: "pending" });
+
+    try {
+      const data = await getTodos(currentFilter);
+      setState({
+        status: "success",
+        data: data.data,
+        info: data.info ?? { all: 0, completed: 0, inWork: 0 }
+      });
+    } catch (error) {
+      setState({ status: "error", error });
+    }
+  };
+
+  useEffect(() => {
+    loadTodoList(filter);
+  }, [filter]);
+
+  // Добавление задачи
+  const addTodo = async (title: string) => {
+    try {
+      await createTodo({ title, isDone: false });
+      await loadTodoList(filter); // Обновляем список задач после добавления новой задачи
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const todos =
+    state.status === "success"
+      ? state.data
+      : [];
+
+  // Счетчики
+  const counts =
+    state.status === "success"
+      ? state.info
+      : { all: 0, completed: 0, inWork: 0 }
+
+  // Удаление задачи
+  const removeTodo = async (id: number) => {
+    try {
+      await deleteTodo(id);
+      await loadTodoList(filter); // Обновляем список задач после удаления задачи
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Изменение задачи
+  const editTodo = async (id: number, title: string) => {
+    try {
+      await updateTodo(id, { title });
+      await loadTodoList(filter); // Обновляем список задач после изменения
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Переключение флага о выполнении задачи
+  const toggleTodo = async (id: number) => {
+    if (state.status !== "success") return;
+
+    const current = state.data.find((todo) => {
+      return todo.id === id
+    });
+
+    try {
+      await updateTodo(id, {
+        isDone: !current?.isDone
+      });
+
+      await loadTodoList(filter); // Обновляем список задач после переключения флага
+    } catch (error) {
+      console.error(error);
+    };
+  };
 
   return (
     <div className="todos">
-      <CreateTodo addTodo={todo.addTodo} />
+      <CreateTodo addTodo={addTodo} />
 
       <TodoFilter
-        filter={todo.filter}
-        setFilter={todo.setFilter}
-        counts={todo.counts}
+        filter={filter}
+        setFilter={setFilter}
+        counts={counts}
       />
 
       <TodoListView
-        todoList={todo.todos}
-        removeTodo={todo.removeTodo}
-        toggleTodo={todo.toggleTodo}
-        editTodo={todo.editTodo}
+        todoList={todos}
+        removeTodo={removeTodo}
+        toggleTodo={toggleTodo}
+        editTodo={editTodo}
       />
     </div>
   )
