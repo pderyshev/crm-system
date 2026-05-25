4005ebab3fd3590fb1c86ea343d7b3f0d859b9f4
pDeryshev <p.deryshev@yandex.ru>
2026-05-25 12:27:27 +0300

    избавился от алертов, заменил на notification

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -3,7 +3,7 @@ import { ToogleCheckbox } from "../ToogleCheckbox/ToogleCheckbox";
 import "./TodoItem.scss"
 import type { Todo } from "../../types/todo";
 import { deleteTodo, updateTodo } from "../../api/todo";
-import { Button, Form } from "antd";
+import { Button, Form, notification } from "antd";
 import { CloseCircleOutlined, DeleteOutlined, EditOutlined, SaveOutlined } from "@ant-design/icons";
 import TodoInput from "../../ui-kit/Input/Input";
 import { MIN_TODO_LENGTH, MAX_TODO_LENGTH } from "../../helpers/validationTitle";
@@ -23,6 +23,7 @@ export const TodoItem: FC<TodoViewProps> = ({
 }) => {
   const [form] = Form.useForm<CreateTodoFormValues>();
   const [isEditing, setIsEditing] = useState(false);
+  const [api, contextHolder] = notification.useNotification();
   const minTitleLength = MIN_TODO_LENGTH
   const maxTitleLength = MAX_TODO_LENGTH
 
@@ -31,9 +32,11 @@ export const TodoItem: FC<TodoViewProps> = ({
     try {
       await deleteTodo(todo.id)
       updateTodoList();
-    } catch (error) {
-      console.error(error);
-      alert("Ошибка при удалении задачи, попробуйте еще раз")
+    } catch {
+      api.error({
+        title: "Ошибка при удалении задачи",
+        description: "Не удалось удалить задачу. Пожалуйста, попробуйте снова."
+      });
     }
   }
 
@@ -60,89 +63,94 @@ export const TodoItem: FC<TodoViewProps> = ({
       setIsEditing(false);
 
       updateTodoList();
-    } catch (error) {
-      console.error(error);
-      alert("Не удалось сохранить изменения, попробуйте еще раз")
+    } catch {
+      api.error({
+        title: "Ошибка при сохранении изменений",
+        description: "Не удалось сохранить изменения. Пожалуйста, попробуйте снова."
+      });
     }
   }
 
   return (
-    <div className="todos__item-wrapper">
-      <div className="todos__item-left">
-        <ToogleCheckbox
-          id={todo.id}
-          isDone={todo.isDone}
-          updateTodoList={updateTodoList}
-        />
-        <div className="todos__edit-warpper">
-          {isEditing ? (
-            <div className="todos__edit-container">
-              <Form
-                form={form}
-                onFinish={handleSaveEdit}
-              >
-                <Form.Item
-                  name="title"
-                  rules={[
-                    { required: true, message: 'Поле не должно быть пустым', },
-                    { min: minTitleLength, message: `Минимальная длина текста ${minTitleLength} символа` },
-                    { max: maxTitleLength, message: `Максимальная длина текста ${maxTitleLength} символов` }
-                  ]}
-                  style={{ marginBottom: 0 }}
+    <>
+      {contextHolder}
+      <div className="todos__item-wrapper">
+        <div className="todos__item-left">
+          <ToogleCheckbox
+            id={todo.id}
+            isDone={todo.isDone}
+            updateTodoList={updateTodoList}
+          />
+          <div className="todos__edit-warpper">
+            {isEditing ? (
+              <div className="todos__edit-container">
+                <Form
+                  form={form}
+                  onFinish={handleSaveEdit}
                 >
-                  <TodoInput />
-                </Form.Item>
-              </Form>
-            </div>
+                  <Form.Item
+                    name="title"
+                    rules={[
+                      { required: true, message: 'Поле не должно быть пустым', },
+                      { min: minTitleLength, message: `Минимальная длина текста ${minTitleLength} символа` },
+                      { max: maxTitleLength, message: `Максимальная длина текста ${maxTitleLength} символов` }
+                    ]}
+                    style={{ marginBottom: 0 }}
+                  >
+                    <TodoInput />
+                  </Form.Item>
+                </Form>
+              </div>
+            ) : (
+              <span
+                className="todos__title"
+                style={{ textDecoration: todo.isDone ? "line-through" : "none" }}
+              >
+                {todo.title}
+              </span>
+            )}
+          </div>
+        </div>
+
+        <div className="todos__inner">
+          {isEditing ? (
+            <>
+              <Button
+                htmlType="submit"
+                type="primary"
+                size="large"
+                icon={<SaveOutlined />}
+                onClick={() => form.submit()}
+              />
+
+              <Button
+                onClick={handleCancelEdit}
+                type="primary"
+                danger
+                size="large"
+                icon={<CloseCircleOutlined />}
+              />
+            </>
           ) : (
-            <span
-              className="todos__title"
-              style={{ textDecoration: todo.isDone ? "line-through" : "none" }}
-            >
-              {todo.title}
-            </span>
+            <>
+              <Button
+                onClick={handleStartEdit}
+                type="primary"
+                size="large"
+                icon={<EditOutlined />}
+              />
+
+              <Button
+                onClick={handleDeleteTodo}
+                type="primary"
+                danger
+                size="large"
+                icon={<DeleteOutlined />}
+              />
+            </>
           )}
         </div>
       </div>
-
-      <div className="todos__inner">
-        {isEditing ? (
-          <>
-            <Button
-              htmlType="submit"
-              type="primary"
-              size="large"
-              icon={<SaveOutlined />}
-              onClick={() => form.submit()}
-            />
-
-            <Button
-              onClick={handleCancelEdit}
-              type="primary"
-              danger
-              size="large"
-              icon={<CloseCircleOutlined />}
-            />
-          </>
-        ) : (
-          <>
-            <Button
-              onClick={handleStartEdit}
-              type="primary"
-              size="large"
-              icon={<EditOutlined />}
-            />
-
-            <Button
-              onClick={handleDeleteTodo}
-              type="primary"
-              danger
-              size="large"
-              icon={<DeleteOutlined />}
-            />
-          </>
-        )}
-      </div>
-    </div>
+    </>
   );
 }
--- src/components/ToogleCheckbox/ToogleCheckbox.tsx
+++ src/components/ToogleCheckbox/ToogleCheckbox.tsx
@@ -1,5 +1,5 @@
 import { type FC } from "react";
-import { Checkbox } from 'antd';
+import { Checkbox, notification } from 'antd';
 import { updateTodo } from "../../api/todo";
 
 export interface CheckboxProps {
@@ -9,20 +9,28 @@ export interface CheckboxProps {
 }
 
 export const ToogleCheckbox: FC<CheckboxProps> = ({ id, isDone, updateTodoList }) => {
+  const [api, contextHolder] = notification.useNotification();
+  
   const handleToogle = async () => {
     try {
       await updateTodo(id, { isDone: !isDone })
       updateTodoList();
-    } catch (error) {
-      console.error(error);
-      alert("Произошла ошибка при смене статуса задачи, повторите попытку")
+    } catch {
+      api.error({
+        title: "Ошибка при смене статуса задачи",
+        description: "Не удалось изменить статус задачи. Пожалуйста, попробуйте снова."
+      });
     }
   }
   
   return (
+    <>
+    {contextHolder}
     <Checkbox
       checked={isDone}
       onChange={handleToogle}
     />
+    </>
+    
   );
 };
